48181312e6015953cae77bcdbe61a2ede206949c
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-04 16:33:43 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
-}
+  const titleCasedTutorials = tutorials.map((tutorial) =>
+    tutorial.replace(/\b\w/g, (l) => l.toUpperCase()),
+);
+return titleCasedTutorials;
+};
